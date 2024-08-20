3a13eee45e62c8a5a779d7a2b456cc3c4310d8a2
SaraToth <saratoth101@gmail.com>
2024-08-20 17:25:36 +0900

    Create function that creates home page content in greeting.js file and then import and call function in index.js on page load to load home page content

--- src/greeting.js
+++ src/greeting.js
@@ -1,1 +1,24 @@
+//import img
+import sandwich from "./sandwich.jpeg";
+
 export const greeting = "Hello";
+
+export function loadHomeContent() {
+    //Create h1
+    const h1 = document.createElement("h1");
+    h1.textContent = "Sara's Sassy Sandwiches";
+
+    //Create img
+    const image = document.createElement("img");
+    image.src = sandwich;
+
+    //Create div text
+    const div = document.createElement("div");
+    div.classList.add("text");
+    div.textContent = "The funny thing is Sara hate's sandwhiches so that explains all the sass.";
+
+    //Add h1, img and div text in order
+    document.body.appendChild(h1);
+    document.body.appendChild(image);
+    document.body.appendChild(div);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { greeting } from "./greeting";
+import { loadHomeContent } from "./greeting";
 
 console.log(greeting);
+
+window.onload(loadHomeContent());
