70bd2f3dcec373b570e44d5d5f5139250fd01de3
SaraToth <saratoth101@gmail.com>
2024-08-20 17:57:20 +0900

    Add classes to nav buttons and then add event listeners to them in index which will be where the logic to toggle page loads will go

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+import "./styles.css";
 import { greeting } from "./greeting";
 import { loadHomeContent } from "./greeting";
 
+
+const homeBtn = document.querySelector(".btn-home");
+const menuBtn = document.querySelector(".btn-menu");
+const contactBtn = document.querySelector(".btn-contact");
+
 console.log(greeting);
 
+//Loads the home page
 window.onload(loadHomeContent());
+
+
+//3 event listeners will toggle content
+menuBtn.addEventListener("click", () => {
+    //clears the DOM
+    //loads the menu page
+});
+
+contactBtn.addEventListener("click", () => {
+    //clears the DOM
+    //loads the contact page
+});
+
+homeBtn.addEventListener("click", () => {
+    //clears the DOM
+    //loads the contact page
+});
