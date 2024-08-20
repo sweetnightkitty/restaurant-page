e69b09d174b361530b8f7394516dc29fafa487eb
SaraToth <saratoth101@gmail.com>
2024-08-20 19:24:58 +0900

    Create makebox and getBoxOne functions on greeting module to create the logic to load the home screen layout divs

--- src/greeting.js
+++ src/greeting.js
@@ -27,6 +27,48 @@ export function loadHomeContent() {
     container.appendChild(image);
     container.appendChild(div);
 
+    //gets first box
+    const boxOne = getBoxOne();
+    container.appendChild(boxOne);
+
+
     //Add container to the content div
     content.appendChild(container);
+}
+
+//May need to refactor this to make content load in the correct place and set up flexbox
+//Turn into a factory? that returns object of div, left and right
+function makeBox() {
+    const div = document.createElement("div");
+    const left = document.createElement("div");
+    const right = left;
+
+    left.classList.add("left-content");
+    right.classList.add("right-content");
+
+    div.appendChild(left);
+    div.appendChild(right);
+
+    return div;
+
+}
+
+
+function getBoxOne () {
+
+    const boxOne = makeBox()
+    boxOne.classList.add("box", "box-one");
+
+    //content is adding to div box but not to right content like intended
+    const h2 = document.createElement("h2");
+    h2.textContent = "Our treat to you"
+
+    const text = document.createElement("div");
+    text.classList.add("text");
+    text.textContent = "Join Butterfly Coffee Rewards and savor our gift to you: a free drink with qualifying purchase during your first week. Valid for one-time use.*";
+
+    boxOne.appendChild(h2);
+    boxOne.appendChild(text);
+
+    return boxOne;
 }
