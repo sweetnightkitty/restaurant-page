a4cf06fa0a218fb12ca97588ffcba7ed24f70454
SaraToth <saratoth101@gmail.com>
2024-08-20 18:02:05 +0900

    Create function in the menu.js module that will create and append a container div to the body

---
+++ src/menu.js
@@ -0,0 +1,8 @@
+function loadMenuPage() {
+
+    //Create container div that houses all content
+    const container = document.createElement("div");
+    container.classList.add("container", "container-menu");
+
+    document.body.appendChild(container);
+}
