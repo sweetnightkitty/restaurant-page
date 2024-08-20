7436113ede7e8c07ad0e9eb1a55301bc3f58c3a9
SaraToth <saratoth101@gmail.com>
2024-08-20 19:03:41 +0900

    Update typos

--- src/greeting.js
+++ src/greeting.js
@@ -4,6 +4,10 @@ import sandwich from "./sandwich.jpeg";
 export function loadHomeContent() {
     const content = document.querySelector("#content");
 
+    //Create container div that houses all home content
+    const container = document.createElement("div");
+    container.classList.add("container", "container-home");
+
     //Create h1
     const h1 = document.createElement("h1");
     h1.textContent = "Butterfly Coffee Company";
@@ -18,8 +22,11 @@ export function loadHomeContent() {
     div.classList.add("text");
     div.textContent = "The funny thing is Sara hate's sandwhiches so that explains all the sass.";
 
-    //Add h1, img and div text in order
-    content.appendChild(h1);
-    content.appendChild(image);
-    content.appendChild(div);
+    //Add h1, img and div text in order to container-home
+    container.appendChild(h1);
+    container.appendChild(image);
+    container.appendChild(div);
+
+    //Add container to the content div
+    content.appendChild(container);
 }
