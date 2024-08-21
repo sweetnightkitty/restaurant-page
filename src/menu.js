0a63c2c5e377b929cd38fd73437f9217ce1c60d5
SaraToth <saratoth101@gmail.com>
2024-08-21 22:33:14 +0900

    Start the logic to create menu page content

--- src/menu.js
+++ src/menu.js
@@ -3,14 +3,58 @@ import { createDivWithTwoClasses } from "./index.js";
 export function loadMenuContent(content) {
     //Create container div that houses all menu content
     const containerMenu = createDivWithTwoClasses("container", "container-menu");
+    const columnLeft = createDivWithTwoClasses("column", "column-left");
+    const columnRight = createDivWithTwoClasses("column", "column-right");
 
-    //Creates h1
-    const h1 = document.createElement("h1");
-    h1.textContent = "Menu Home!";
+    columnLeft.textContent = "Drinks";
+    columnRight.textContent = "Menu";
 
-    //add h1 to menu container
-    containerMenu.appendChild(h1);
+    //add  to menu container
+    containerMenu.appendChild(columnLeft);
+    containerMenu.appendChild(columnRight);
 
     //add menu container to the page content
     content.appendChild(containerMenu);
 };
+
+function createNav() {
+    const nav = document.createElement("nav");
+
+    const drinksMenu = document.createElement("ul");
+    createMiniMenu("Drinks", (drinks.length), drinks, nav);
+}
+
+const drinks = ["Iced Energy", "Hot Coffees", "Cold Coffees", "Starbucks Refreshers Beverages", "Frappuccino Blended Beverages", "Iced Tea & Lemonade", "Hot Teas", "Milk, Juice & More", "Bottled Beverages"];
+
+function createMiniMenu(menuTitle, itemsNumber, array, nav) {
+    const span = document.createElement("span");
+    span.textContent = menuTitle;
+    nav.appendChild(span);
+
+    for(let i = 0; i < itemsNumber; i++) {
+        const li = document.createElement("li");
+        li.textContent = array[i];
+        nav.appendChild(li);
+    }
+
+    return nav;
+
+}
+
+
+
+{/* <nav>
+    <ul>
+        <li>
+            <span>Drinks</span>
+            <ul>
+                <li>
+                    <a ="" href="#">Drink type</a>
+                </li>
+            </ul>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav> */}
