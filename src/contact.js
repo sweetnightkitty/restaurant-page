8fef2712a790a2c2ca5e4cab0520d3daaa082677
SaraToth <saratoth101@gmail.com>
2024-08-21 21:55:45 +0900

    Finish styling contact page content, update contact.js to change paraOne to an h1 instead, and add extra divs to paragraph three

--- src/contact.js
+++ src/contact.js
@@ -5,16 +5,18 @@ export function loadContactContent(content) {
 
     //Houses all contact content
     const containerContact = createDivWithTwoClasses("container", "container-contact");
+    const h1 = document.createElement("h1");
+    h1.textContent = "Contact us";
 
     //Creates Paragraphs
-    const paraOne = makeParagraph("one");
+    // const paraOne = makeParagraph("one");
     const paraTwo = makeParagraph("two")
     const paraThree = makeParagraph("three");
     const paraFour = makeParagraph("four");
     const paraFive = makeParagraph("five");
 
     // //Appends paragraphs to containerContact
-    containerContact.appendChild(paraOne);
+    containerContact.appendChild(h1);
     containerContact.appendChild(paraTwo);
     containerContact.appendChild(paraThree);
     containerContact.appendChild(paraFour);
@@ -32,32 +34,36 @@ function makeParagraph(number) {
     const storedContent = [
         {
             paragraph: "one",
-            title: "title",
-            text: "text",
+            title: "Contact us",
+            text: "",
         },
 
         {
             paragraph: "two",
-            title: "title",
-            text: "text",
+            title: "Customer Service",
+            text: 'If you have general questions or concerns about Starbucks, please contact our Customer Contact Center <a href="#">customerservice.starbucks.com</a>',
         },
 
         {
             paragraph: "three",
-            title: "title",
-            text: "text",
+            title: "Media Contacts",
+            text: "Please email your detailed inquiry and deadline for response to the following: <br>Global:",
+            textTwo: "Phone: 206 318 7100",
+            textThree: 'Email: <a href="#">press@starbucks.com</a> <br>For inquiries related to Asia or Canada please visit their respective Starbucks Stories & News site <br>Europe, Middle East and Africa',
+            textFour: "Phone +44 20 3047 2656",
+            textFive: "Email: PressEMEA@starbucks.com",
         },
 
         {
             paragraph: "four",
-            title: "title",
-            text: "text",
+            title: "Investor Relations",
+            text: 'For inquiries related to stock ownership please submit a written inquiry through email to <a href="#">investorrelations@starbucks.com</a>.',
         },
 
         {
             paragraph: "five",
-            title: "title",
-            text: "text",
+            title: "Partnership and Sponsorship Requests",
+            text: 'If you have an exciting partnership or sponsorship you’d like us to consider, please send a detailed overview of the opportunity and your information to <a href="#">partnerships@starbucks.com</a>.',
         },
     ];
 
@@ -77,15 +83,37 @@ function makeParagraph(number) {
 
     //create h2 and div that will hold paragraph text
     const h2 = document.createElement("h2");
-    const div = createDivWithTwoClasses();
+    const div = createDivWithTwoClasses("text", `text-${number}`);
 
     //Assigns text based on the respective paragraph
     h2.textContent = storedContent[index].title;
-    div.textContent = storedContent[index].text;
 
-    //Appends text to the paragraph div
+    if(index == 2) {
+        const divOne = createDivWithTwoClasses("text", `text-${number}-one`);
+        const divTwo = createDivWithTwoClasses("text", `text-${number}-two`);
+        const divThree = createDivWithTwoClasses("text", `text-${number}-three`);
+        const divFour = createDivWithTwoClasses("text", `text-${number}-four`);
+        const divFive = createDivWithTwoClasses("text", `text-${number}-five`);
+
+        divOne.innerHTML = storedContent[index].text;
+        divTwo.innerHTML = storedContent[index].textTwo;
+        divThree.innerHTML = storedContent[index].textThree;
+        divFour.innerHTML = storedContent[index].textFour;
+        divFive.innerHTML = storedContent[index].textFive;
+
+        paragraph.appendChild(h2);
+        paragraph.appendChild(divOne);
+        paragraph.appendChild(divTwo);
+        paragraph.appendChild(divThree);
+        paragraph.appendChild(divFour);
+        paragraph.appendChild(divFive);
+    } else {
+        div.innerHTML = storedContent[index].text;
+            //Appends text to the paragraph div
     paragraph.appendChild(h2);
     paragraph.appendChild(div);
+
+    }
 
     return paragraph;
 };
