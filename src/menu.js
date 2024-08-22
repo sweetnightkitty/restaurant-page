c7a63fb6d384f57b69c6d1c9ed6bf56d36a5f418
SaraToth <saratoth101@gmail.com>
2024-08-22 15:52:36 +0900

    Finish the create Nav function and the make subMenu function that generates all of the content on the left column nav bar

--- src/menu.js
+++ src/menu.js
@@ -6,8 +6,10 @@ export function loadMenuContent(content) {
     const columnLeft = createDivWithTwoClasses("column", "column-left");
     const columnRight = createDivWithTwoClasses("column", "column-right");
 
-    columnLeft.textContent = "Drinks";
     columnRight.textContent = "Menu";
+
+    const nav = createNav();
+    columnLeft.appendChild(nav);
 
     //add  to menu container
     containerMenu.appendChild(columnLeft);
@@ -18,43 +20,109 @@ export function loadMenuContent(content) {
 };
 
 function createNav() {
-    const nav = document.createElement("nav");
+    //Creates the nav and the fullMenu inside
+    const nav = createElementWithTwoClasses("nav", "nav-menu");
+    const fullMenu = createElementWithTwoClasses("ul", "menu", "menu-main");
 
-    const drinksMenu = document.createElement("ul");
-    createMiniMenu("Drinks", (drinks.length), drinks, nav);
-}
+    //Creates the four sub menus
+    const subMenuOne = createSubMenu("one");
+    const subMenuTwo = createSubMenu("two");
+    const subMenuThree = createSubMenu("three");
+    const subMenuFour = createSubMenu("four");
 
-const drinks = ["Iced Energy", "Hot Coffees", "Cold Coffees", "Starbucks Refreshers Beverages", "Frappuccino Blended Beverages", "Iced Tea & Lemonade", "Hot Teas", "Milk, Juice & More", "Bottled Beverages"];
+    //Appends sub menus to the full menu
+    fullMenu.appendChild(subMenuOne);
+    fullMenu.appendChild(subMenuTwo);
+    fullMenu.appendChild(subMenuThree);
+    fullMenu.appendChild(subMenuFour);
 
-function createMiniMenu(menuTitle, itemsNumber, array, nav) {
-    const span = document.createElement("span");
-    span.textContent = menuTitle;
-    nav.appendChild(span);
-
-    for(let i = 0; i < itemsNumber; i++) {
-        const li = document.createElement("li");
-        li.textContent = array[i];
-        nav.appendChild(li);
-    }
+    //Appends the full menu to the nav
+    nav.appendChild(fullMenu);
 
     return nav;
-
-}
-
+};
 
 
-{/* <nav>
-    <ul>
-        <li>
-            <span>Drinks</span>
-            <ul>
-                <li>
-                    <a ="" href="#">Drink type</a>
-                </li>
-            </ul>
-        </li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-</nav> */}
+
+function createSubMenu(number) {
+    //Each array contains one subMenu's title and list of products
+    //Each array at index 0 is the menu title; index 1+ is a menu item
+    const drinks = [
+        "Drinks",
+        "Iced Energy",
+        "Hot Coffees",
+        "Cold Coffees",
+        "Starbucks Refreshers Beverages",
+        "Frappuccino Blended Beverages",
+        "Iced Tea & Lemonade",
+        "Hot Teas",
+        "Milk, Juice & More",
+        "Bottled Beverages",
+    ];
+
+    const food = [
+        "Food",
+        "Hot Breakfast",
+        "Oatmeal & Yogurt",
+        "Bakery",
+        "Lunch",
+        "Snacks & Sweets",
+    ];
+
+    const homeCoffee = [
+        "At Home Coffee",
+        "Whole Bean",
+        "VIA Instant",
+    ];
+
+    const merchandise = [
+        "Merchandise",
+        "Cold Cups",
+        "Tumblers",
+        "Mugs",
+        "Other",
+    ];
+
+    //Sets the current array to the correct sub Menu info
+    let currentMenuArray;
+    if(number == "one") {
+        currentMenuArray = drinks;
+    } else if(number == "two") {
+        currentMenuArray = food;
+    } else if(number == "three") {
+        currentMenuArray = homeCoffee;
+    } else if(number == "four") {
+        currentMenuArray = merchandise;
+    };
+
+    //Creates sub menu and sub menu's title
+    const subMenu = createElementWithTwoClasses("li", "sub-menu", `sub-menu-${number}`);
+    const subMenuTitle = createElementWithTwoClasses("span", "sub-menu-title", `sub-menu-title-${number}`);
+    
+    //Assigns the Title which is the first item in the array
+    subMenuTitle.textContent = currentMenuArray[0];
+
+    const subMenuItems = createElementWithTwoClasses("ul");
+
+    //Generates all the menu items and appends them to sub menu items ul
+    for(let i = 1; i < currentMenuArray.length; i++) {
+        const item = createElementWithTwoClasses("li", "menu-item", `menu-item-${number}`);
+        item.textContent = currentMenuArray[i];
+        subMenuItems.appendChild(item);
+    };
+
+    //Appends menu title and menu items to the sub menu
+    subMenu.appendChild(subMenuTitle);
+    subMenu.appendChild(subMenuItems);
+
+    return subMenu;
+
+};
+
+
+
+function createElementWithTwoClasses(type, classOne, classTwo) {
+    const tempElement = document.createElement(type);
+    tempElement.classList.add(classOne, classTwo);
+    return tempElement;
+};
